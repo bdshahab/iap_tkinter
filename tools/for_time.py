4ff26f6f256a91af79d0898a8d9b4289a825e6ef
Shahab Baradaran Dilmaghani <bdshahab@gmail.com>
2025-05-14 12:33:32 +0330

    Update for_time.py

--- tools/for_time.py
+++ tools/for_time.py
@@ -1,4 +1,4 @@
 def get_display_time(number_seconds: int):
     sec = number_seconds % 60
     minute = number_seconds // 60
-    return "%02d:%02d" % (minute, sec)
+    return f"{minute:02}:{sec:02}"
