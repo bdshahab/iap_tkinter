9e227445946afb93c0a9f2c3edad2fcea8dc5c9d
Shahab Baradaran Dilmaghani <bdshahab@gmail.com>
2025-05-15 10:57:22 +0330

    Update iap_variables.py

--- Payment/iap_variables.py
+++ Payment/iap_variables.py
@@ -6,7 +6,7 @@ Set its parameter to "true" to test.
 Also, the payment type must be Litecoin.
 https://blockchair.com/litecoin/transaction/61a7667851da2d1395c26f4eaba7a14a3c1355ba80e1b35678327619a115d21e
 """
-TESTING = not False
+TESTING = False
 PRICE_TEST_IS_OK = False
 DATE_TEST_IS_OK = False
 TIME_TEST_IS_OK = False
